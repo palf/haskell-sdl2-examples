4a0cd0a5fa4cfc9dc06c0113958859712856a3ef
Jack Palfrey <jack.palf@gmail.com>
2015-07-17 23:07:44 +0100

    updating lesson 06; wip

--- src/lesson06.hs
+++ src/lesson06.hs
@@ -1,127 +1,51 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Main where
 
 import qualified Graphics.UI.SDL as SDL
 import qualified Graphics.UI.SDL.Image as Image
 import Graphics.UI.SDL.Types
-import Control.Monad
-import Data.Bits
-import Foreign.C.String
-import Foreign.C.Types
-import Foreign.Marshal.Alloc
-import Foreign.Marshal.Utils
-import Foreign.Ptr
-import Foreign.Storable
-import GHC.Word
+import Shared.Assets
+import Shared.Input
+import Shared.Lifecycle
+import Shared.Polling
+import Shared.Utils
 
 
-type Risky a = Either String a
+title :: String
+title = "lesson06"
 
+size :: ScreenSize
+size = (640, 480)
 
-lessonTitle :: String
-lessonTitle = "lesson06"
-
-screenWidth :: CInt
-screenWidth = 640
-
-screenHeight :: CInt
-screenHeight = 480
+inWindow :: (SDL.Window -> IO ()) -> IO ()
+inWindow = withSDL . withWindow title size
 
 fullWindow :: SDL.Rect
 fullWindow = SDL.Rect {
     rectX = 0,
     rectY = 0,
-    rectW = screenWidth,
-    rectH = screenHeight }
-
+    rectW = fst size,
+    rectH = snd size }
 
 main :: IO ()
-main = do
-    initializeSDL [SDL.SDL_INIT_VIDEO] >>= catchRisky
-    window <- createWindow lessonTitle >>= catchRisky
+main = inWindow $ \window -> do
     initializeSDLImage [Image.InitPNG] >>= catchRisky
-
     screenSurface <- SDL.getWindowSurface window
     pixelFormat <- SDL.surfaceFormat `applyToPointer` screenSurface
-
-    loadedSurface <- loadSurface "./assets/loaded.png" >>= catchRisky
+    loadedSurface <- getSurfaceFrom "./assets/loaded.png"
     imageSurface <- convertSurface loadedSurface pixelFormat 0 >>= catchRisky
-
     let draw surface = SDL.blitScaled surface nullPtr screenSurface nullPtr >> SDL.updateWindowSurface window
-
-    repeatUntilComplete $ draw imageSurface >> handle pollEvent
-
+    repeatUntilComplete $ draw imageSurface >> handleSimple pollEvent
     SDL.freeSurface loadedSurface
     SDL.freeSurface imageSurface
-    SDL.destroyWindow window
-    SDL.quit
     Image.quit
-
 
 catchRisky :: Risky a -> IO a
 catchRisky = either throwSDLError return
-
-
-initializeSDL :: [Word32] -> IO (Risky CInt)
-initializeSDL flags = do
-    initSuccess <- SDL.init $ foldl (.|.) 0 flags
-    return $ if initSuccess < 0 then Left "SDL could not initialize!" else Right initSuccess
-
 
 initializeSDLImage :: [Image.InitFlag] -> IO (Risky CInt)
 initializeSDLImage flags = do
     initSuccess <- Image.init $ Image.initFlagsToC flags
     return $ if initSuccess < 0 then Left "SDL_image could not initialize!" else Right initSuccess
 
-
-createWindow :: String -> IO (Risky SDL.Window)
-createWindow windowTitle = withCAString windowTitle $ \title -> do
-    window <- SDL.createWindow title SDL.SDL_WINDOWPOS_UNDEFINED SDL.SDL_WINDOWPOS_UNDEFINED screenWidth screenHeight SDL.SDL_WINDOW_SHOWN
-    return $ if window == nullPtr then Left "Window could not be created!" else Right window
-
-
-loadSurface :: String -> IO (Risky (Ptr SDL.Surface))
-loadSurface path = do
-    surface <- withCAString path Image.load
-    return $ if surface == nullPtr then Left ("Unable to load image " ++ path ++ "!") else Right surface
-
-
-convertSurface :: Ptr Surface -> Ptr PixelFormat -> Word32 -> IO (Risky (Ptr SDL.Surface))
-convertSurface surface format flags = do
-    optimizedSurface <- SDL.convertSurface surface format flags
-    return $ if optimizedSurface == nullPtr then Left "Unable to optimize image!" else Right optimizedSurface
-
-
-throwSDLError :: String -> IO a
-throwSDLError message = do
-    errorString <- SDL.getError >>= peekCString
-    fail (message ++ " SDL_Error: " ++ errorString)
-
-
-repeatUntilComplete :: IO Bool -> IO ()
-repeatUntilComplete operation = do
-    complete <- operation
-    unless complete $ repeatUntilComplete operation
-
-
-handle :: IO (Maybe SDL.Event) -> IO Bool
-handle stream = do
-    maybeEvent <- stream
-    case maybeEvent of
-        Nothing -> return False
-
-        Just (SDL.QuitEvent _ _) -> return True
-
-        _ -> return False
-
-
-pollEvent :: IO (Maybe SDL.Event)
-pollEvent = alloca $ \pointer -> do
-    status <- SDL.pollEvent pointer
-
-    if status == 1
-        then maybePeek peek pointer
-        else return Nothing
-
-
-applyToPointer :: (Storable a) => (a -> b) -> Ptr a -> IO b
-applyToPointer operation pointer = liftM operation $ peek pointer
