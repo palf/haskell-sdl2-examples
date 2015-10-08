9d59c01a5c3e29fc61264941ed9ed7f8672e0a5d
Jack Palfrey <jack.palf@gmail.com>
2015-10-08 10:55:14 +0100

    some refactoring to support adoption of reader

--- src/shared/drawing.hs
+++ src/shared/drawing.hs
@@ -1,12 +1,23 @@
-module Shared.Drawing where
+module Shared.Drawing (
+    withBlankScreen
+) where
 
 import qualified Graphics.UI.SDL as SDL
+import Control.Monad
 
 
 withBlankScreen :: SDL.Renderer -> IO a -> IO ()
 withBlankScreen renderer operation = do
+    clearScreen renderer
+    _ <- operation
+    drawToScreen renderer
+
+clearScreen :: SDL.Renderer -> IO ()
+clearScreen renderer = do
     _ <- SDL.setRenderDrawColor renderer 0xFF 0xFF 0xFF 0xFF
     _ <- SDL.renderClear renderer
-    _ <- operation
-    _ <- SDL.renderPresent renderer
     return ()
+
+drawToScreen :: SDL.Renderer -> IO ()
+drawToScreen renderer = void $ SDL.renderPresent renderer
+
