372ba39ad2d96f1a187a39f772a104686f98c566
Jack Palfrey <jack.palf@gmail.com>
2017-09-30 01:34:26 +0100

    adding example 07

--- common/lib/Common.hs
+++ common/lib/Common.hs
@@ -6,6 +6,8 @@ import Control.Monad          (void)
 import Control.Monad.IO.Class (MonadIO)
 import Data.Text              (Text)
 
+import SDL (($=))
+
 
 withSDL :: (MonadIO m) => m a -> m ()
 withSDL op = do
@@ -15,15 +17,29 @@ withSDL op = do
 
 
 withWindow :: (MonadIO m) => Text -> (Int, Int) -> (SDL.Window -> m a) -> m ()
-withWindow title (w, h) op = do
-  window <- SDL.createWindow title p
-  SDL.showWindow window
-  void $ op window
-  SDL.destroyWindow window
+withWindow title (x, y) op = do
+  w <- SDL.createWindow title p
+  SDL.showWindow w
+  void $ op w
+  SDL.destroyWindow w
 
     where
       p = SDL.defaultWindow { SDL.windowInitialSize = z }
-      z = SDL.V2 (fromIntegral w) (fromIntegral h)
+      z = SDL.V2 (fromIntegral x) (fromIntegral y)
+
+
+withRenderer :: (MonadIO m) => SDL.Window -> (SDL.Renderer -> m a) -> m ()
+withRenderer w op = do
+  r <- SDL.createRenderer w (-1) rendererConfig
+  void $ op r
+  SDL.destroyRenderer r
+
+
+rendererConfig :: SDL.RendererConfig
+rendererConfig = SDL.RendererConfig
+  { SDL.rendererType = SDL.AcceleratedVSyncRenderer
+  , SDL.rendererTargetTexture = False
+  }
 
 
 renderSurfaceToWindow :: (MonadIO m) => SDL.Window -> SDL.Surface -> SDL.Surface -> m ()
@@ -35,3 +51,7 @@ renderSurfaceToWindow w s i
 isQuitEvent :: SDL.Event -> Bool
 isQuitEvent (SDL.Event _t SDL.QuitEvent) = True
 isQuitEvent _ = False
+
+
+setHintQuality :: (MonadIO m) => m ()
+setHintQuality = SDL.HintRenderScaleQuality $= SDL.ScaleNearest
---
+++ examples/lesson07/src/Lesson07.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import qualified SDL
+import qualified SDL.Image
+import qualified Common as C
+
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Extra (whileM)
+
+
+main :: IO ()
+main = C.withSDL $ do
+  C.setHintQuality
+  C.withWindow "Lesson 07" (640, 480) $ \w ->
+    C.withRenderer w $ \r -> do
+
+      tex <- SDL.Image.loadTexture r "./assets/texture.png"
+
+      whileM $
+        isContinue <$> SDL.pollEvent
+        >>= conditionallyRun (draw r tex)
+
+
+draw :: (MonadIO m) => SDL.Renderer -> SDL.Texture -> m ()
+draw r t = do
+  SDL.clear r
+  SDL.copy r t Nothing Nothing
+  SDL.present r
+
+
+isContinue :: Maybe SDL.Event -> Bool
+isContinue Nothing = True
+isContinue (Just e) = not $ C.isQuitEvent e
+
+
+conditionallyRun :: (Monad m) => m a -> Bool -> m Bool
+conditionallyRun f True = const True <$> f
+conditionallyRun _ False = pure False
