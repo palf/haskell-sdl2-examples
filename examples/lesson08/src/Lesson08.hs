d980fd9dc5a4fad2ebef2b7cbbd184bd6e5eff05
Jack Palfrey <jack.palf@gmail.com>
2023-03-13 21:23:54 +0000

    using reader instead of argument for renderer in lesson08

--- examples/lesson08/src/Lesson08.hs
+++ examples/lesson08/src/Lesson08.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ConstraintKinds   #-}
+{-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
@@ -7,6 +9,7 @@ import qualified SDL
 
 import           Control.Monad.Extra    (whileM)
 import           Control.Monad.IO.Class (MonadIO)
+import           Control.Monad.Reader   (MonadReader, ask, runReaderT)
 import           Data.Word
 import           Foreign.C.Types        (CInt)
 import           SDL                    (($=))
@@ -15,38 +18,45 @@ import           SDL                    (($=))
 data Colour = White | Red | Blue | Green | Yellow
 
 
-setColor :: (MonadIO m) => SDL.Renderer -> Colour -> m ()
-setColor r c  = SDL.rendererDrawColor r $= getColor c
+type HasRenderer m = ( MonadIO m, MonadReader SDL.Renderer m)
+
+
+setColour :: (HasRenderer m) => Colour -> m ()
+setColour c = do
+  r <- ask
+  SDL.rendererDrawColor r $= getColour c
+
   where
-    getColor :: Colour -> SDL.V4 Word8
-    getColor White  = SDL.V4 maxBound maxBound maxBound maxBound
-    getColor Red    = SDL.V4 maxBound 0 0 maxBound
-    getColor Green  = SDL.V4 0 maxBound 0 maxBound
-    getColor Blue   = SDL.V4 0 0 maxBound maxBound
-    getColor Yellow = SDL.V4 maxBound maxBound 0 maxBound
+    getColour :: Colour -> SDL.V4 Word8
+    getColour White  = SDL.V4 maxBound maxBound maxBound maxBound
+    getColour Red    = SDL.V4 maxBound 0 0 maxBound
+    getColour Green  = SDL.V4 0 maxBound 0 maxBound
+    getColour Blue   = SDL.V4 0 0 maxBound maxBound
+    getColour Yellow = SDL.V4 maxBound maxBound 0 maxBound
 
 
-clearScreen :: (MonadIO m) => SDL.Renderer -> m ()
-clearScreen r = do
-  setColor r White
+clearScreen :: (HasRenderer m) => m ()
+clearScreen = do
+  r <- ask
+  setColour White
   SDL.clear r
 
 
-drawRectangle :: (MonadIO m) => SDL.Renderer -> SDL.Rectangle CInt -> m ()
-drawRectangle r s = SDL.drawRect r (Just s)
+drawRectangle :: (HasRenderer m) => SDL.Rectangle CInt -> m ()
+drawRectangle s = ask >>= \r -> SDL.drawRect r (Just s)
 
 
-fillRectangle :: (MonadIO m) => SDL.Renderer -> SDL.Rectangle CInt -> m ()
-fillRectangle r s = SDL.fillRect r (Just s)
+fillRectangle :: (HasRenderer m) => SDL.Rectangle CInt -> m ()
+fillRectangle s = ask >>= \r -> SDL.fillRect r (Just s)
 
 
-drawLine :: (MonadIO m) => SDL.Renderer -> (CInt, CInt) -> (CInt, CInt) -> m ()
-drawLine r (ox, oy) (tx, ty) =
-  SDL.drawLine r (C.mkPoint ox oy) (C.mkPoint tx ty)
+drawLine :: (HasRenderer m) => (CInt, CInt) -> (CInt, CInt) -> m ()
+drawLine (ox, oy) (tx, ty) =
+  ask >>= \r -> SDL.drawLine r (C.mkPoint ox oy) (C.mkPoint tx ty)
 
 
-drawDot :: (MonadIO m) => SDL.Renderer -> (CInt, CInt) -> m ()
-drawDot r (x, y) = SDL.drawPoint r (SDL.P (SDL.V2 x y))
+drawDot :: (HasRenderer m) => (CInt, CInt) -> m ()
+drawDot (x, y) = ask >>= \r -> SDL.drawPoint r (SDL.P (SDL.V2 x y))
 
 
 mkRect :: t -> t -> t -> t -> SDL.Rectangle t
@@ -64,23 +74,19 @@ screenHeight :: (Num a) => a
 screenHeight = 480
 
 
-draw :: (MonadIO m) => SDL.Renderer -> m ()
-draw r = do
-  clearScreen r
-  withColor Red    >> fillRectangle' innerRect
-  withColor Green  >> drawRectangle' outerRect
-  withColor Blue   >> drawLine' (0, screenHeight `div` 2) (screenWidth, screenHeight `div` 2)
-  withColor Yellow >> mapM_ (\y -> drawDot' (screenWidth `div` 2, y)) [ 0, 4 .. screenHeight ]
-  SDL.present r
+draw :: (HasRenderer m) => m ()
+draw = do
+  clearScreen
+  setColour Red    >> fillRectangle innerRect
+  setColour Green  >> drawRectangle outerRect
+  setColour Blue   >> drawLine (0, screenHeight `div` 2) (screenWidth, screenHeight `div` 2)
+  setColour Yellow >> mapM_ (\y -> drawDot (screenWidth `div` 2, y)) [ 0, 4 .. screenHeight ]
+
+  ask >>= SDL.present
 
   where
     innerRect = mkRect (screenWidth `div` 4) (screenHeight `div` 4) (screenWidth `div` 2) (screenHeight `div` 2)
     outerRect = mkRect (screenWidth `div` 6) (screenHeight `div` 6) (2 * screenWidth `div` 3) (2 * screenHeight `div` 3)
-    withColor = setColor r
-    fillRectangle' = fillRectangle r
-    drawRectangle' = drawRectangle r
-    drawLine' = drawLine r
-    drawDot' = drawDot r
 
 
 main :: IO ()
@@ -89,10 +95,10 @@ main = C.withSDL $ do
   C.withWindow "Lesson 08" (screenWidth, screenHeight) $ \w ->
     C.withRenderer w $ \r -> do
 
-      setColor r White
+      runReaderT (setColour White) r
 
       whileM $ do
         ev <- SDL.pollEvents
         if C.hasQuitEvent ev
           then pure False
-          else draw r >> pure True
+          else runReaderT draw r >> pure True
